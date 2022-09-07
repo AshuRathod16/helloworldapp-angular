7332e36d96c016b7286f197bea6ea7a755de19ec
Qa-krushna <rashwini510@gmail.com>
2022-09-07 12:07:35 +0530

    Added: Using input event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,9 @@ export class AppComponent {
   title = 'HelloWorld';
   imageUrl="../assets/logo.jpg";
   url="https://www.bridgelabz.com";
-  userName: string = "Ashwini";
+  userName: string = "";
+  nameError: string = "";
+  
 
   ngOnInit(): void{
     this.title = "Hello From Bridgelabz.";
@@ -18,5 +20,16 @@ export class AppComponent {
   onClick( $event: any ){
     console.log("Save button is clicked !", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event:any){
+    console.log("change Event occured!", $event.data);
+    const nameRegexP = RegExp('^[A-Z]{1}[A-Za-z//s]{2,}$');
+    if(nameRegexP.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError ="Name is Incorrect";
+  
   }
 }
